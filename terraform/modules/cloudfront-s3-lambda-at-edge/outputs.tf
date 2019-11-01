561312364b57b11a5ddae58acae35b7127f42a50
Hector Castro <hectcastro@gmail.com>
2019-10-31 23:19:00 -0400

    Clean up module and project outputs

--- terraform/modules/cloudfront-s3-lambda-at-edge/outputs.tf
+++ terraform/modules/cloudfront-s3-lambda-at-edge/outputs.tf
@@ -1,3 +1,4 @@
-output "domain_name" {
-  value = "${aws_cloudfront_distribution.cdn.domain_name}"
+output "cdn_endpoint" {
+  value       = "${aws_cloudfront_distribution.cdn.domain_name}"
+  description = "CloudFront endpoint responsible for fronting the contents of an S3 bucket."
 }
