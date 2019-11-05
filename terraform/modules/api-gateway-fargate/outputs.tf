9d659f059ec72519df0011468fb50dd0af974505
Hector Castro <hectcastro@gmail.com>
2019-11-05 12:23:57 -0500

    WIP

--- terraform/modules/api-gateway-fargate/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "api_endpoint" {
-  value       = "${aws_api_gateway_deployment.default.invoke_url}"
-  description = "API Gateway endpoint responsible for proxying requests to the ECS service."
-}
-
-output "ecs_security_group_id" {
-  value       = "${aws_security_group.default.id}"
-  description = "Security group ID of the ECS service security group."
-}
